ccb044f6dce039cf19d8d5a1d9e9586094b08012
darin-codes <booakadarim@gmail.com>
2025-09-07 23:17:22 +0000

    Finshed M1T1!

--- m1t1_090x.cpp
+++ m1t1_090x.cpp
@@ -4,5 +4,24 @@ M1T1 for the online sections
 blankenshipd
 9/7/2025
 Program will greet the user.
+
+IF it happens:
+"stage changes" , no, push directly
+"commit and sync", always
+"pull?" doesn't matter
 */
 
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+    //input: start the program
+    //processing: none
+    //output: 0 (no erors)
+    //special FX: Says "Hello" to the user
+
+    // Let's say hello
+    cout << "Hello, user!" << endl; // "end line" not "end 1"
+    return 0; // no errors
+}
